492051904b842113833276ff3b1996a36173db67
Maiori44 <felice.dangelo2013@gmail.com>
2023-02-22 12:51:39 +0100

    Add conf.lua

---
+++ conf.lua
@@ -0,0 +1,16 @@
+function love.conf(t)
+	t.version = "11.4"
+	t.window.title = "IP please"
+	--t.window.icon = ""
+	t.window.resizable = true
+	t.window.minwidth = 800
+	t.window.minheight = 600
+	
+	t.modules.image = false
+	t.modules.joystick = false
+	t.modules.mouse = false
+	t.modules.thread = false
+	t.modules.physics = false
+	t.modules.touch = false
+	t.modules.video = false
+end
