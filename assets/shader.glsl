261579f4bea558cd205d74133e40e3702659be7f
Maiori44 <felice.dangelo2013@gmail.com>
2023-02-24 20:01:09 +0100

    add icon

--- assets/shader.glsl
+++ assets/shader.glsl
@@ -2,7 +2,7 @@ vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) 
 	float v = 0.5 * (sin(texture_coords.y * 3.14159 / 1 * love_ScreenSize.y + 1) + 1.0);
 	color.rgb -= -color.rgb * (pow(v, 0.1) - 1.0);
 	texture_coords = texture_coords * 2.0 - vec2(1.0);
-	texture_coords += (texture_coords.yx * texture_coords.yx) * texture_coords * vec2(0.0015, 0.001625);
+	texture_coords += (texture_coords.yx * texture_coords.yx) * texture_coords * vec2(0.003, 0.003250);
 	float mask = (1.0 - smoothstep(0.98, 1.0, abs(texture_coords.x)))
 				* (1.0 - smoothstep(0.98, 1.0, abs(texture_coords.y)));
 	texture_coords = (texture_coords + vec2(1.0)) / 2.0;
--- conf.lua
+++ conf.lua
@@ -1,12 +1,11 @@
 function love.conf(t)
 	t.version = "11.4"
 	t.window.title = "IP please"
-	--t.window.icon = ""
+	t.window.icon = "icon.png"
 	t.window.resizable = true
 	t.window.minwidth = 800
 	t.window.minheight = 600
 	
-	t.modules.image = false
 	t.modules.joystick = false
 	t.modules.mouse = false
 	t.modules.thread = false
